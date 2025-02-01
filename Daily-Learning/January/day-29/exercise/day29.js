1fdcd19b63e0bb15bd03441ea8cacd03267b5cd7
Jayson07 <jayelegant007@gmail.com>
2025-02-01 20:45:33 +0100

    day32_updated

---
+++ Daily-Learning/January/day-29/exercise/day29.js
@@ -0,0 +1,10 @@
+// Hands on deck
+
+const callToAction = () => {
+    const message = 'Dear Creedian, i wish you all the best in your endearvous and great success in your exams.No matter the hurdles, keep pusing forward! And ensure to submit your assignments on TimeRanges.Stay focus and keep winning!';
+
+console.log(message);
+alert(message);
+};
+
+callToAction();
